2ca8123e91729ca51b285a6241f4877e9fc94d5c
kriztofa <christopherokeke700@gmail.com>
2024-01-12 20:25:08 -0800

    none

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * implementation of _puts
+ * _puts - ouputs a string to the standard output
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	void _puts(char *str)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
